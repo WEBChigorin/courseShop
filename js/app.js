704e7ef62e5fe467c3a4c9c4fb80e85953b31fed
MikleMikle <mmchigorin@gmail.com>
2018-09-23 16:33:40 +0300

    add function that put the data into the shopping cart section in the header

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //variables
 
 const courses = document.querySelector('#courses-list');
+const shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 //Event Listeners
@@ -25,7 +26,7 @@ function byCourse(event) {
     //Use Delegation to find the course that was added
     if(event.target.classList.contains('add-to-cart')) {
         // read the course values
-        const course = event.target.parentElement.parentElement;
+        const course = event.target.parentElement.parentElement; //div 'card'
 
         //read the values
         getCourseInfo(course);
@@ -34,5 +35,36 @@ function byCourse(event) {
 
 //Reads the HTML information of the selected course
 function getCourseInfo(course) {
-    
+
+    //Create an Object with Course Data
+    const courseInfo = {
+        image: course.querySelector('img').src, // get the src of the image
+        title: course.querySelector('h4').textContent, //get the text from the h4
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id') //get the id of the a
+    }
+    //Insert into the shopping cart
+    addIntoCart(courseInfo);
+}
+// Display the selected course into the shopping cart
+
+function addIntoCart(course) {   // course = courseInfo from previous func.()
+    //create a <tr>
+    const row = document.createElement('tr');
+
+    //Build the template. course = courseInfo object
+    row.innerHTML = `
+        <tr>
+            <td>
+                <img src="${course.image}">  
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+        </tr>
+    `;
+    //Add into the shopping cart 
+    shoppingCartContent.appendChild(row);
 }
